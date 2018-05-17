097178ef490043e12b82fdc5820f642841924d37
YingboMa <mayingbo5@gmail.com>
2018-05-16 21:41:52 -0700

    HW5: first draft

---
+++ HW5/plot.jl
@@ -0,0 +1,32 @@
+using Plots
+pgfplots()
+
+y = x = -20:5.0:20
+pts = vec(P2[(x[i], x[j]) for i=1:length(x), j=1:length(x)])
+df(x, y) = [1, x].*0.2
+xlim=(minimum(x), maximum(x))
+ylim=(minimum(y), maximum(y))
+plt = quiver(pts, quiver=df, xlims=xlim, ylims=ylim, size=(250,250))
+para(t) = [t, t^2/2 - 17]
+t = -20:0.2:20
+plot!(plt, map(x->para(x)[1], t), map(x->para(x)[2], t))
+savefig("fig.tex")
+
+f(x,y) = begin
+    sin.(x) + cos.(y)
+end
+X = repmat(x',length(y),1)
+Y = repmat(y,1,length(x))
+Z = map(f,X,Y)
+p = contour(x,y,Z)
+savefig("fig1.pdf")
+run(`google-chrome-stable fig1.pdf`)
+
+n = 100
+ts = linspace(0,2,n)
+x = ts .* map(cos,ts)
+y = (0.1ts)
+z = collect(1:n) ./ 100
+plot(x,y,z,fill=true,leg=false,cbar=false,w=1, size=(250,250))
+savefig("fig1.tex")
+run(`google-chrome-stable fig1.pdf`)
